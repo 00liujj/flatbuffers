c26d00fd6b5d1e2296d6fb0d550666d87845ea4a
Jianjun Liu <00liujj@163.com>
2018-05-13 20:49:52 +0800

    change define to typedef

--- cpp98_compat/flatbuffers/compat_defs.h
+++ cpp98_compat/flatbuffers/compat_defs.h
@@ -4,14 +4,27 @@
 #define nullptr 0
 #define noexcept
 #define constexpr
-#define intptr_t int*
-#define uintptr_t unsigned int*
-#define uint8_t  unsigned char
-#define uint16_t unsigned short
-#define uint32_t unsigned int
-#define uint64_t unsigned long
-#define uintmax_t unsigned long
+
+typedef int* intptr_t;
+typedef unsigned int* uintptr_t;
+typedef long ptrdiff_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+typedef unsigned long uintmax_t;
+
+//typedef char int8_t;
+typedef short int16_t;
+//typedef int int32_t;
+typedef long int64_t;
+typedef long intmax_t;
+
+
 #define static_assert(x, y)
 #endif
 
+#include <string.h>
+#include <stdlib.h>
 
